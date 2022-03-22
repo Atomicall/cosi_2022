430648eda0ee23c356f2ec0708fbbaba27eaa796
Atomicall <60043149+Atomicall@users.noreply.github.com>
2022-03-22 11:12:05 +0300

    2 done

--- 2convolution and correlation/2CC/main.cpp
+++ 2convolution and correlation/2CC/main.cpp
@@ -30,12 +30,12 @@ float signalZ(float x)
 	return cos(cosCoeff * x);
 	}
 
-void doubleWrite(vector<double>& what, std::ofstream& where, std::string head)
+void doubleWrite(vector<double>& what, std::ofstream& where, std::string head, vector<double>& x, int size)
 	{
 	where << head << "\n";
-	for (size_t i = 0; i < what.size(); i++)
+	for (size_t i = 0; i < size; i++)
 		{
-		where << std::fixed << what[i] << " ";
+		where << std::fixed << x [i] <<";" <<  what[i] << "\n";
 		}
 	}
 void writeToFIles(vector<double>& X, vector<double>& Y, vector<double>& Z, vector<double>& Conv, vector<double>& Corr)
@@ -51,11 +51,11 @@ void writeToFIles(vector<double>& X, vector<double>& Y, vector<double>& Z, vecto
 	std::ofstream file4("conv.txt");
 	std::ofstream file5("corr.txt");
 
-	doubleWrite(X, file1, "");
-	doubleWrite(Y, file2, "");
-	doubleWrite(Z, file3, "");
-	doubleWrite(Conv, file4, "");
-	doubleWrite(Corr, file5, "");
+	doubleWrite(X, file1, "", X, X.size());
+	doubleWrite(Y, file2, "", X, Y.size());
+	doubleWrite(Z, file3, "", X, Z.size());
+	doubleWrite(Conv, file4, "", X, X.size());
+	doubleWrite(Corr, file5, "", X, X.size());
 	
 	file1.close();
 	file2.close();
@@ -63,7 +63,22 @@ void writeToFIles(vector<double>& X, vector<double>& Y, vector<double>& Z, vecto
 	file4.close();
 	file5.close();
 	}
+void vectToVectComplex(vector<double>& what, vector<complex<double>>& whereTo)
+	{
+	for (int i = 0; i < what.size(); i++)
+		{
+		whereTo[i] = (complex<double>(what[i]));
+		}
+	}
 
+void vectComplexToVectDoubles(vector<complex<double>>& what, vector<double>& whereTo)
+	{
+	for (int i = 0; i < what.size(); i++)
+		{
+		whereTo[i] = ((what[i].real()));
+		//whereTo[i] = std::abs(what[i]);
+		}
+	}
 void conv(vector<double>& A, vector<double>& B, vector<double>& result)
 	{
 	//https://toto-share.com/2011/11/cc-convolution-source-code/
@@ -126,10 +141,15 @@ void fft_time(vector<complex<double>>& arr)
 	{
 	const size_t N = arr.size();
 	if (N <= 1) return;
-
-	vector<complex<double>> even = arr[std::slice(0, N / 2, 2)];
-	vector<complex<double>> odd = arr[std::slice(1, N / 2, 2)];
-
+	vector<complex<double>> even(N/2);
+	vector<complex<double>> odd(N/2);
+	for (int i = 0, j=0, k=0; i < arr.size(); i++)
+		{
+		if (i % 2 == 0)
+			even[j] = arr[i], j++;
+		else
+			odd[k] = arr[i], k++;
+		}
 	fft_time(even);
 	fft_time(odd);
 
@@ -156,11 +176,32 @@ void ifft(vector<complex<double>>& x)
 		*it = std::conj((*it));
 		}
 	// scale the numbers
-	x /= x.size();
 	for (auto it = x.begin(); it != x.end(); it++)
 		{
-		*it = *it / x.size();
+		*it /= x.size();
 		}
+	}
+
+
+void convolution_fft(vector < complex < double >>& A, vector < complex < double >>& B, vector <complex < double >>& result)
+	{
+	const size_t N = A.size();
+	for (int i = 0; i < N; i++)
+		result[i] = A[i] * B[i];
+	ifft(result);
+	}
+void correlation_fft(vector < complex < double >>& A, vector < complex < double >>& B, vector < complex < double >>& result)
+	{
+	const size_t N = A.size();
+	vector < complex < double >> conjugated (A.size());
+	int i = 0;
+	for ( auto it = conjugated.begin(); it != conjugated.end(); it++, i++)
+		{
+		*it = std::conj(A[i]);
+		}
+	for (int i = 0; i < N; i++)
+		result[i] = conjugated[i] * B[i];
+	ifft(result);
 	}
 
 
@@ -171,6 +212,7 @@ int main()
 	vector<double> functionZ;
 	float stepSize = (2 * M_PI )/ N_O;
 	int index = 0;
+	//for (float i = 0; i < 2* M_PI; i += stepSize)
 	for (float i = 0; i < 2* M_PI; i += stepSize)
 		{
 		signalFunctionX.push_back(i);
@@ -184,24 +226,64 @@ int main()
 
 	vector<double> XYConv(functionY.size() + functionZ.size()-1);
 	vector<double> XYCorr(functionY.size() + functionZ.size()-1);
+	vector<double> XYConvFFT(functionY.size());
+	vector<double> XYCorrFFT(functionY.size());
 
 	conv(functionY, functionZ, XYConv);
 	corr(functionY, functionZ, XYCorr);
 
-	writeToFIles(signalFunctionX, functionY, functionZ, XYConv, XYCorr);
+	vector<complex<double>> tmpFunY(functionY.size());
+	vector<complex<double>> tmpFunZ(functionZ.size());
+	vector<complex<double>> tmpFunYForCorr(functionY.size());
+	vector<complex<double>> tmpFunZForCorr(functionZ.size());
+	vectToVectComplex(functionY, tmpFunY);
+	vectToVectComplex(functionZ, tmpFunZ);
+	fft_time(tmpFunY);
+	fft_time(tmpFunZ);
+	vector<complex<double>> XYConvFFTTemp(XYConvFFT.size());
+ 	convolution_fft(tmpFunY, tmpFunZ, XYConvFFTTemp);
+	vectComplexToVectDoubles(XYConvFFTTemp, XYConvFFT);
+
+
+	vector<complex<double>> XYCorrFFTTemp(functionY.size());
+	vectToVectComplex(functionY, tmpFunYForCorr);
+	vectToVectComplex(functionZ, tmpFunZForCorr);
+	correlation_fft(tmpFunYForCorr, tmpFunZForCorr, XYCorrFFTTemp);
+	vectComplexToVectDoubles(XYCorrFFTTemp, XYCorrFFT);
+
+
+
+	writeToFIles(signalFunctionX, functionY, functionZ, XYConvFFT, XYCorr);
 #ifdef DBG
 	std::cout << "XYConv " << "\n";
-	for (int i = 0; i < XYConv.size(); i++)
+	for (int i = 0; i < XYConv.size() /2 + 1; i++)
 		{
 		std::cout << XYConv[i] << " ; ";
 		if (!(i%3))	std::cout << "\n";
 		}
 	std::cout << "\n";
 #endif
-
 #ifdef DBG
 	std::cout << "XYCorrelatoin " << "\n";
-	for (int i = 0; i < XYCorr.size(); i++)
+	for (int i = 0; i < XYCorr.size()/2 +1 ; i++)
+		{
+		std::cout << XYCorr[i] << " ; ";
+		if (!(i % 3))	std::cout << "\n";
+		}
+	std::cout << "\n";
+#endif
+#ifdef DBG
+	std::cout << "XYConvFFT " << "\n";
+	for (int i = 0; i < XYConvFFT.size(); i++)
+		{
+		std::cout << XYConv[i] << " ; ";
+		if (!(i % 3))	std::cout << "\n";
+		}
+	std::cout << "\n";
+#endif
+#ifdef DBG
+	std::cout << "XYCorrFFT " << "\n";
+	for (int i = 0; i < XYCorrFFT.size(); i++)
 		{
 		std::cout << XYCorr[i] << " ; ";
 		if (!(i % 3))	std::cout << "\n";
